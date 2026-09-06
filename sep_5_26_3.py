0b6a6be385772bb3bc6e6b6f2a3fcedeaef21171
arul <arulfranciswork@gmail.com>
2026-09-05 18:29:01 -0700

    Create sep_5_26_3.py
    accept inputs from the user:
      - food: "meatloaf"
      - question: "how many calories"
    create the prompt 
    call deep seek LLM with the prompt
    print the answer

---
+++ sep_5_26_3.py
@@ -0,0 +1,33 @@
+# import lib
+import os 
+from dotenv import load_dotenv
+from openai import OpenAI
+
+# set api key 
+api_key = os.environ.get('DEEPSEEK_API_KEY')
+
+# connect to llm 
+client = OpenAI(api_key=api_key, 
+	base_url="https://api.deepseek.com")
+
+# accept user input vars
+food= input('What food are you interested in: ')
+question= input('What is your question: ')
+
+# define prompt
+prompt=f""" {question} in {food}. Keep the answers short. Less than 50 words. Format the answers nicely. One short sentence per line."""
+
+# call llm 
+response=client.chat.completions.create(
+	model="deepseek-v4-flash",
+	messages=[{
+	"role":"system",
+	"content":"you are a nutrition assistant. Answer the user's nutrition question directly and briefly."
+	},{
+	"role":"system",
+	"content":prompt
+	}])
+
+# print response
+answer = response.choices[0].message.content
+print(answer)
