6442335997fa5cb61580ae16c5c3f29c441b4458
arul <arulfranciswork@gmail.com>
2026-09-12 20:10:34 -0700

    Update sep_12_26_4.sql
    remove redundant intermediate table: social_posts

--- sep_12_26_4.sql
+++ sep_12_26_4.sql
@@ -20,19 +20,9 @@ select * from ANALYSIS_SEP_2026.SOCIAL.SOCIAL_POSTS_RAW limit 3;
 3	Honestly, Found a quiet little neighborhood that wasn't in any guidebook.
 */
 
--- 2. move down from SOCIAL_POSTS_RAW -> SOCIAL_POSTS using SQL
--- check rowcount in social_posts
-INSERT INTO social_posts (post_id, post_text)
-SELECT post_id, post_text
-FROM social_posts_raw;
+-- 2. move data from SOCIAL_POSTS -> SOCIAL_POSTS_SENTIMENT using python
 
-
-SELECT COUNT(*)
-FROM social_posts; 
-
--- 4. move data from SOCIAL_POSTS -> SOCIAL_POSTS_SENTIMENT using python
-
--- 5. verify final target table SOCIAL_POSTS_SENTIMENT
+-- 3. verify final target table SOCIAL_POSTS_SENTIMENT
 
 SELECT *
 FROM SOCIAL_POSTS_SENTIMENT
